6de29d66e31c521ef851637eaf334dd96e0bd423
Axel Estrada <axele1524@gmail.com>
2021-02-14 10:11:13 -0600

    Version estable 1.02

--- main.go
+++ main.go
@@ -5,9 +5,8 @@ import "fmt"
 func main(){
 	var edad int8 = 17
 	var peso int8 = 125
-	comida := "Cafe con pan"
 	var nombre string = "Axel"
 	var perro string = "Luna"
 
-	fmt.Println(nombre, edad, "Peso:", peso, "Perro:", perro)
+	fmt.Println("Nombre:", nombre, "edad:", edad, "Peso:", peso, "Perro:", perro)
 }
